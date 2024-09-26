637d997064f9a823773a3e566446120f9d8c835f
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-09-26 17:52:15 +0200

    feat: replace reference with import

--- src/models/project.ts
+++ src/models/project.ts
@@ -1,8 +1,6 @@
-namespace App {
-    export enum ProjectStatus { Active, Finsihed}
-    
-    export class Project {
-        constructor(public id: string, public title: string, public description: string, public people: number, public status: ProjectStatus) {
-        }
+export enum ProjectStatus { Active, Finsihed}
+
+export class Project {
+    constructor(public id: string, public title: string, public description: string, public people: number, public status: ProjectStatus) {
     }
 }
